9f2d97aed5ff3727939aeb4feef50a07cad0773c
Pandeigede2803 <dedegetto_47@yahoo.com>
2023-05-21 01:24:13 +0800

    tugas katalog marketplace

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html',
-'index-form.html'
+'index-form.html',
+'indexgrid.html',
+'indexflex.html',
+'marketplace.html',
+
 ],
   theme: {
     extend: {},
