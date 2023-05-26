1b56107c5254a1fdcda07007dac54c35660532e6
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2023-05-26 14:39:05 +0800

    projectdesktop2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,24 @@ module.exports = {
 'indexflex.html',
 'marketplace.html',
 'blog.html',
+'card.html',
+'project.html',
+'project2.html',
 
 ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-1': '#0c134f',
+        'blue-2': '#1d267d',
+        'blue-3': '#1d387d',
+        'blue-4': '#2f58cd',
+        'grey-1': '#d9d9d9',
+        'merah-1': '#de6363',
+        'kuning': '#ffd95a',
+        
+      },
+    },
   },
   plugins: [],
 }
